ca58f90c3fcad25eda2a1d6a5ea84a302f86caab
molihua21 <169095562+molihua21@users.noreply.github.com>
2024-05-06 15:26:40 -0500

    Create molihua21.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/molihua21.py
@@ -0,0 +1,8 @@
+#https://www.python.org
+# es la primera vez que hago una practica de programacion
+***
+Dia 1
+aprendo cosas que nunca he hecho
+***
+my_variable= ¨ humedad ¨
+my_variable= ¨ 35 ¨  
